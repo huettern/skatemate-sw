84f488d2a44a358d12e3032e4c8f2cb91c949f25
Noah Huetter <noahhuetter@gmail.com>
2017-04-04 18:03:22 +0200

    CMSIS DSP benchmark

--- motorcontrol/main.c
+++ motorcontrol/main.c
@@ -27,8 +27,10 @@
 
 #include "usbcdc.h"
 #include "usbcfg.h"
+#include "chprintf.h"
 
 // #include "core_cm4_simd.h"
+#include "arm_math.h"
 
 /*===========================================================================*/
 /* settings                                                                */
@@ -96,28 +98,51 @@ int main(void)
   res = __UADD8(op1,op2);
 
   // wait 10sec
-  chThdSleepMilliseconds(10000);
-  utlmEnable(true);
-
-  while (true) 
+  
+  
+  
+  // CMSIS benchmark
+  systime_t ticks, pidtime, sintime;
+  arm_pid_instance_f32 pid;
+  float32_t a, b;
+  arm_pid_init_f32(&pid, 1);
+  while(true)
   {
-    chThdSleepMilliseconds(1000);
-    utlmSend(0, 10, xdata, ydata);
-    for(i = 0; i < 10; i ++)
-    {
-      xdata[i] = xdata[i] + 10;
-    }
-    
-    xda++;
-    yda = 1.0;
-    utlmSend(1, 1, &xda, &yda);
+    ticks = chVTGetSystemTimeX();
+    for(op1 = 0; op1 < 1000; op1++) arm_pid_f32(&pid,10.0f);
+    pidtime = chVTGetSystemTimeX();
+    for(op1 = 0; op1 < 1000; op1++) arm_sin_cos_f32(29.8, &a, &b);
+    sintime = chVTGetSystemTimeX();
 
 
-    palTogglePad(GPIOE,14);
+    chprintf(bssusb, "PID time: %fus \r\n", (float)(pidtime-ticks)/CH_CFG_ST_FREQUENCY*1000);
+    chprintf(bssusb, "SINCOS time: %fus \r\n", (float)(sintime-pidtime)/CH_CFG_ST_FREQUENCY*1000);
 
-
-    // usbcdcHandleShell();
+    chThdSleepMilliseconds(2000);
   }
+
+  
+  // chThdSleepMilliseconds(10000);
+  // utlmEnable(true);
+  // while (true) 
+  // {
+  //   chThdSleepMilliseconds(1000);
+  //   utlmSend(0, 10, xdata, ydata);
+  //   for(i = 0; i < 10; i ++)
+  //   {
+  //     xdata[i] = xdata[i] + 10;
+  //   }
+    
+  //   xda++;
+  //   yda = 1.0;
+  //   utlmSend(1, 1, &xda, &yda);
+
+
+  //   palTogglePad(GPIOE,14);
+
+
+  //   // usbcdcHandleShell();
+  // }
 }
 
 /** @} */
